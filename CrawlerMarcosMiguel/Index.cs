6afa069872e764f5b86beded81b6ed286a4637fa
gabriel.franca <gabriel.franca@ntendencia.com.br>
2019-10-21 15:50:28 -0300

    All

---
+++ CrawlerMarcosMiguel/Index.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrawlerMarcosMiguel
+{
+    public class Index
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
